98570361af4ec71bc46b8b0aacd61436d09ccb64
Matheus Mendes <matheusmendesc.16@gmail.com>
2024-02-18 17:39:26 -0300

    GET All products paged

--- src/main/java/com/matheusmendesdev/Dscommerce/services/ProductService.java
+++ src/main/java/com/matheusmendesdev/Dscommerce/services/ProductService.java
@@ -3,9 +3,13 @@ package com.matheusmendesdev.Dscommerce.services;
 import com.matheusmendesdev.Dscommerce.dto.ProductDTO;
 import com.matheusmendesdev.Dscommerce.entities.Product;
 import com.matheusmendesdev.Dscommerce.repositories.ProductRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -13,8 +17,15 @@ public class ProductService {
     @Autowired
     private ProductRepository repository;
 
+    @Transactional(readOnly = true)
     public ProductDTO findById(Long id){
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+        Page<Product> product = repository.findAll(pageable);
+        return product.map(x -> new ProductDTO(x));
     }
 }
