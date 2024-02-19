29171ba636a9d4d34cbb613a88136ae36744e1e3
Matheus Mendes <matheusmendesc.16@gmail.com>
2024-02-18 23:54:58 -0300

    Exception handling

---
+++ src/main/java/com/matheusmendesdev/Dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.matheusmendesdev.Dscommerce.controllers.handlers;
+
+import com.matheusmendesdev.Dscommerce.dto.CustomError;
+import com.matheusmendesdev.Dscommerce.services.exceptions.DataBaseException;
+import com.matheusmendesdev.Dscommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<CustomError> database(DataBaseException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
--- src/main/java/com/matheusmendesdev/Dscommerce/services/ProductService.java
+++ src/main/java/com/matheusmendesdev/Dscommerce/services/ProductService.java
@@ -3,10 +3,15 @@ package com.matheusmendesdev.Dscommerce.services;
 import com.matheusmendesdev.Dscommerce.dto.ProductDTO;
 import com.matheusmendesdev.Dscommerce.entities.Product;
 import com.matheusmendesdev.Dscommerce.repositories.ProductRepository;
+import com.matheusmendesdev.Dscommerce.services.exceptions.DataBaseException;
+import com.matheusmendesdev.Dscommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -19,7 +24,7 @@ public class ProductService {
 
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id){
-        Product product = repository.findById(id).get();
+        Product product = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Resource Not Found!"));
         return new ProductDTO(product);
     }
 
@@ -39,15 +44,27 @@ public class ProductService {
 
     @Transactional
     public ProductDTO update(Long id, ProductDTO dto){
+        try{
         Product entity = repository.getReferenceById(id);
         copyDtoToEntity(entity, dto);
         entity = repository.save(entity);
         return new ProductDTO(entity);
+        }
+        catch (EntityNotFoundException ex){
+            throw new ResourceNotFoundException("Resource Not Found!");
+        }
     }
 
-    @Transactional
+    @Transactional(propagation = Propagation.SUPPORTS)
     public void delete(Long id){
-        repository.deleteById(id);
+        if(!repository.existsById(id)){
+            throw new ResourceNotFoundException("Resouce Not Found!");
+        }
+        try{
+            repository.deleteById(id);
+        }catch (DataIntegrityViolationException ex){
+            throw new DataBaseException("Fail reference integration");
+        }
     }
 
     private void copyDtoToEntity(Product entity, ProductDTO dto) {
